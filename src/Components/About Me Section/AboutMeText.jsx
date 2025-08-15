8f95aca5e973e3f458569d93829ba9ceaac274d4
Neeraj chaudhary <nerajchaudhary948@gmail.com>
2025-08-15 11:50:43 +0530

    Update project

--- src/Components/About Me Section/AboutMeText.jsx
+++ src/Components/About Me Section/AboutMeText.jsx
@@ -8,7 +8,7 @@ export const AboutMeText = () => {
         </h2>
         <p className='text-white'>
             Hello! I'm Neeraj, currently enrolled in the third year (fifth semester) of a 3-year Diploma in Engineering (specializing in Computer Science / Information Technology) at a polytechnic institute in Uttar Pradesh. The diploma programme is designed around hands-on, industry-oriented learning, covering modules like data structures, operating systems, networking, web technologies and end-semester project work. This structure makes diploma holders job-ready after year-three and also enables lateral entry into the second year of a B.Tech or direct IT roles.
-            <button className='border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan hover:shadow-small '  onClick={() => window.open("https://drive.google.com/file/d/1PjeQx4qsSil4HeV6hz5LtICbFErq9hDM/view?usp=drivesdk", "_blank")}>My CV</button>
+            <button className='border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan hover:shadow-small '  onClick={() => window.open("https://drive.google.com/file/d/1QA7OtwUjf0uMCeTbLoT4BNXf2MJ3wj5Q/view?usp=drivesdk", "_blank")}>My CV</button>
         </p>
     </div>
   )
